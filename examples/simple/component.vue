ea30925ab9ba35e68bcb6be8ec8c4df7fe3129e0
dozo <dohzoh@outlook.com>
2018-07-25 22:41:49 +0900

    add readme sample

--- examples/simple/component.vue
+++ examples/simple/component.vue
@@ -1,34 +1,12 @@
 <template>
-    <div class="box">
-		<div>{{text}}</div>
-		<a class="link" href="#" v-text="text"></a>
-	</div>
+  <div>
+      {{text}}
+  </div>
 </template>
-
 <script>
-        var result = {
-            data: function() {
-                return {"text": "Ok from component.vue using scoped css"};
-            }
-        };
-
-        // return to be used in unit tests 
-        export default result;
+export default {
+  data: function() {
+    return { text: "Ok" };
+  }
+};
 </script>
-
-<style scoped>
-
-.box {
-	height: 50px;
-}
-
-.link:hover {
-    content: '';
-	  color: red;
-}
-div {
-    border: 1px solid;
-}
-
-</style>
-/* vim: set tabstop=4 softtabstop=4 shiftwidth=4 noexpandtab : */
--- examples/simple/rollup.config.js
+++ examples/simple/rollup.config.js
@@ -1,22 +1,24 @@
 import typescript from "rollup-plugin-typescript2";
 
-export default {
-  input: "./app.ts",
-  output: {
-    file: "app.js",
-    format: "esm"
-  },
-  plugins: [
-    typescript({
-      tsconfigOverride: {
-        compilerOptions: {
-          allowSyntheticDefaultImports: true,
-          moduleResolution: "node",
-          module: "es2015",
-          lib: ["es2015"],
-          types: ["node", "systemjs"]
+export default [
+  {
+    input: "./app.ts",
+    output: {
+      file: "app.js",
+      format: "esm"
+    },
+    plugins: [
+      typescript({
+        tsconfigOverride: {
+          compilerOptions: {
+            allowSyntheticDefaultImports: true,
+            moduleResolution: "node",
+            module: "es2015",
+            lib: ["es2015"],
+            types: ["node", "systemjs"]
+          }
         }
-      }
-    })
-  ]
-};
+      })
+    ]
+  }
+];
